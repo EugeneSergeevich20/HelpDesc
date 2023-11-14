471e3800055afb421de84c1b761703a6a56709e0
EugeneSergeevich20 <eugene.chernitsyn15@gmail.com>
2023-11-14 18:54:13 +0400

    First commit

---
+++ src/main/java/com/helpdesc/HelpDescApplication.java
@@ -0,0 +1,13 @@
+package com.helpdesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDescApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpDescApplication.class, args);
+    }
+
+}
